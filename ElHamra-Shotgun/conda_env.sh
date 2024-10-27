8060af3d88ece4588612a737d6c2b3defc5d165e
Raafat A. Eissa <76243557+RaafatA@users.noreply.github.com>
2024-10-27 12:03:55 +0300

    Update conda_env.sh

--- ElHamra-Shotgun/conda_env.sh
+++ ElHamra-Shotgun/conda_env.sh
@@ -4,6 +4,5 @@ conda config --add channels defaults
 conda config --add channels bioconda
 conda config --add channels conda-forge
 
-conda create -n shotgun fastp fastqc multiqc trimmomatic samtools bowtie megahit prokka humann prodigal 
-
-source activate shotgun
+conda create -n meta-shotgun snakemake fastqc trimmomatic multiqc bowtie2 samtools megahit quast prokka prodigal humann eggnog-mapper
+conda activate meta-shotgun
